666af5fe056e41e8569aa23edb28f21a7171ba21
Sh1bari <vova_krasnov_2004@mail.ru>
2023-10-28 11:35:11 +0300

    init backend

---
+++ src/main/java/com/example/marsohodbackcontrolcenter/MarsohodBackControlCenterApplication.java
@@ -0,0 +1,13 @@
+package com.example.marsohodbackcontrolcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsohodBackControlCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarsohodBackControlCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/marsohodbackcontrolcenter/MarsohodBackControlCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marsohodbackcontrolcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsohodBackControlCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
