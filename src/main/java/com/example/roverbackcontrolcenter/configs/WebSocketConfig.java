3444220c39d27a1005016257e131a878ee8285ab
Sh1bari <vova_krasnov_2004@mail.ru>
2023-10-29 05:08:24 +0300

    fix exHandler

--- src/main/java/com/example/roverbackcontrolcenter/configs/WebSocketConfig.java
+++ src/main/java/com/example/roverbackcontrolcenter/configs/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.example.roverbackcontrolcenter.configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -14,6 +15,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
  */
 @Configuration
 @EnableWebSocket
+@CrossOrigin
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
